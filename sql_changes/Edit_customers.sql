425428744d64ab0aedcc49d100f8710991358ab5
camilajaviera <cjmunozn@hotmail.com>
2025-01-07 18:27:40 -0300

    feat(edit_sql): change the column type.

--- sql_changes/Edit_customers.sql
+++ sql_changes/Edit_customers.sql
@@ -1,4 +1,4 @@
-ALTER TABLE clothing_store.customers ALTER COLUMN customer_id TYPE text USING customer_id::text;
 ALTER TABLE clothing_store.customers ALTER COLUMN "name" TYPE varchar USING "name"::varchar;
-ALTER TABLE clothing_store.customers ALTER COLUMN phone TYPE varchar USING phone::varchar;
 ALTER TABLE clothing_store.customers ALTER COLUMN email TYPE varchar USING email::varchar;
+ALTER TABLE clothing_store.customers ALTER COLUMN phone TYPE varchar USING phone::varchar;
+ALTER TABLE clothing_store.customers ALTER COLUMN address TYPE varchar USING address::varchar;
