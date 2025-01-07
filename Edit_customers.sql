586d3a59fb9feeaf36551f2a26a16d21e3993bd5
camilajaviera <cjmunozn@hotmail.com>
2025-01-07 18:10:13 -0300

    delete file

--- Edit_customers.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE clothing_store.customers ALTER COLUMN customer_id TYPE text USING customer_id::text;
-ALTER TABLE clothing_store.customers ALTER COLUMN "name" TYPE varchar USING "name"::varchar;
-ALTER TABLE clothing_store.customers ALTER COLUMN phone TYPE varchar USING phone::varchar;
-ALTER TABLE clothing_store.customers ALTER COLUMN email TYPE varchar USING email::varchar;
