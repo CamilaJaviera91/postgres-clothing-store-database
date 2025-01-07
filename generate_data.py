60c5e66762a02d5d6d8743ff52241ae3863d14b3
camilajaviera <cjmunozn@hotmail.com>
2025-01-07 17:57:59 -0300

    fix(generate): create file so we can create the data that we need.

--- generate_data.py
+++ generate_data.py
@@ -9,13 +9,17 @@ num_entries = 500
 # Generate the data
 customers = []
 for _ in range(num_entries):
-    name = fake.name()
-    email = fake.email()
-    phone = fake.phone_number()
-    address = fake.address().replace('\n', ', ')
-    customers.append((name, email, phone, address))
+    customer_id = fake.uuid4()  # Unique ID
+    name = fake.name().replace("'", "''")  # Escape single quotes
+    email = fake.email().replace("'", "''")
+    phone = fake.phone_number().replace("'", "''")
+    address = fake.address().replace("\n", ", ").replace("'", "''")
+    date = fake.date_between(start_date='-10y', end_date='today')  # Date in YYYY-MM-DD format
+    customers.append((customer_id, name, email, phone, address, date))
 
 # Print the data in SQL format
-print("INSERT INTO clothing_store.customers (name, email, phone, address) VALUES")
-values = ",\n".join([f"    ('{c[0]}', '{c[1]}', '{c[2]}', '{c[3]}')" for c in customers])
+print("INSERT INTO clothing_store.customers (cutomer_id, name, email, phone, address, registration_date) VALUES")
+values = ",\n".join(
+    [f"    ('{c[0]}', '{c[1]}', '{c[2]}', '{c[3]}', '{c[4]}', '{c[5]}')" for c in customers]
+)
 print(values + ";")
